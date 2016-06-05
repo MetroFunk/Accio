7ecc0798c7725117714e7634404fe918439d123e
MetroFunk <nazca070@hotmail.com>
2016-06-04 18:55:56 -0600

    Add files via upload

--- Accio/src/main/java/com/tec/accio/app/Main.java
+++ Accio/src/main/java/com/tec/accio/app/Main.java
@@ -7,5 +7,13 @@ package com.tec.accio.app;
  */
 public class Main {
 
+    public static void main(String[] args) throws Exception {
+
+        AccioIndexer accio = new AccioIndexer();
+        accio.read_documents_array("/home/metrofunko/Desktop/Compiladores-FunctionalCare/Accio/filesToIndex");
+        accio.frequency_generator();
+        accio.matrix_generator();
+        accio.printMatrix();
+    }
 
 }
