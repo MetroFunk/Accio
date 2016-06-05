aebbf72af9368d16f22d8d1947b2cc9b952369c2
MetroFunk <nazca070@hotmail.com>
2016-06-04 18:54:51 -0600

    Add files via upload

--- Accio/src/main/java/com/tec/accio/app/AccioIndexer.java
+++ Accio/src/main/java/com/tec/accio/app/AccioIndexer.java
@@ -1,94 +1,135 @@
 package com.tec.accio.app;
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
+
 /**
  * Created by metrofunko on 04/06/16.
  */
 public class AccioIndexer {
 
-    List<String> stopwords = Arrays.asList("a");
 
-    Map<String, List<Tuple>> index = new HashMap<String, List<Tuple>>();
-    List<String> files = new ArrayList<String>();
 
-    public void indexFile(File file) throws IOException {
-        int fileno = files.indexOf(file.getPath());
-        if (fileno == -1) {
-            files.add(file.getPath());
-            fileno = files.size() - 1;
-        }
+    private HashMap<String, Double> term_frequency_in_collection = new HashMap<String, Double>();
+    private HashMap<String, HashMap<Integer, Double>> term_frequency_in_files = new HashMap<String, HashMap<Integer, Double>>();
+    private Vector<String> documents_array=new Vector<String>();
+    private Vector<String> list_of_terms = new Vector<String>();
 
-        int pos = 0;
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        for (String line = reader.readLine(); line != null; line = reader
-                .readLine()) {
-            for (String _word : line.split("\\W+")) {
-                String word = _word.toLowerCase();
-                pos++;
-                if (stopwords.contains(word))
-                    continue;
-                List<Tuple> idx = index.get(word);
-                if (idx == null) {
-                    idx = new LinkedList<Tuple>();
-                    index.put(word, idx);
-                }
-                idx.add(new Tuple(fileno, pos));
+
+    public double[][] matrix;
+
+    public String readOneFile(File f){
+        StringBuffer page=new StringBuffer();
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+            String line =" ";
+            while((line=br.readLine())!=null){
+                page.append(line);
+                page.append(" ");
             }
+
+        }catch(Exception e){
+            e.printStackTrace();
         }
-        System.out.println("indexed " + file.getPath() + " " + pos + " words");
+        return page.toString();
     }
 
-    public void search(List<String> words) {
-        for (String _word : words) {
-            Set<String> answer = new HashSet<String>();
-            String word = _word.toLowerCase();
-            List<Tuple> idx = index.get(word);
-            if (idx != null) {
-                for (Tuple t : idx) {
-                    answer.add(files.get(t.fileno));
-                }
-            }
-            System.out.print(word);
-            for (String f : answer) {
-                System.out.print(" " + f);
-            }
-            System.out.println("");
-        }
-    }
- /*
-    public static void main(String[] args) {
-        try {
-            AccioIndexer idx = new AccioIndexer();
-            idx.indexFile(new File("/home/zeru/IdeaProjects/Accio/Accio/filesToIndex/quack.txt"));
+    public  void read_documents_array(String dir){
+        try{
 
-            idx.search(Arrays.asList("/home/zeru/IdeaProjects/Accio/Accio/filesToIndex/quack.txt".split(",")));
-        } catch (Exception e) {
+            File f=new File(dir);
+            File files[]=f.listFiles();
+            System.out.println("Reading"+files.length+" files");
+            String line=" ";
+            for(File filePath: files){
+                String fileContent = readOneFile(filePath);
+                documents_array.add(fileContent);
+            }
+
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
-*/
-    private class Tuple {
-        private int fileno;
-        private int position;
 
-        public Tuple(int fileno, int position) {
-            this.fileno = fileno;
-            this.position = position;
-        }
+    public String[] tokenize(String text){
+        String terms[]=text.split("[^a-zA-Z0-9]+");
+        return terms;
     }
 
+    public void frequency_generator(){
+        for(int i=0;i<documents_array.size();i++){
+            String page=documents_array.get(i).toLowerCase();
 
+            String terms[]=tokenize(page);
+
+            for(int j=0;j<terms.length;j++){
+                if(terms[j].isEmpty())continue;
+
+                double val=1;
+                if(term_frequency_in_collection.get(terms[j])!=null)
+                    val = val+term_frequency_in_collection.get(terms[j]);
+                term_frequency_in_collection.put(terms[j], val);
+
+                double val2=1;
+                HashMap<Integer, Double> tempInner = new HashMap<Integer, Double>();
+                if(term_frequency_in_files.get(terms[j])!=null)
+                    tempInner = term_frequency_in_files.get(terms[j]);
+
+                if(tempInner.get(i)!=null)
+                    val2 = val2+tempInner.get(i);
+
+                tempInner.put(i, val2);
+                term_frequency_in_files.put(terms[j], tempInner);
+            }
+        }
+        list_of_terms.addAll(term_frequency_in_collection.keySet());
+        System.out.println(term_frequency_in_collection);
+    }
+
+    public  void matrix_generator(){
+        matrix = new double[documents_array.size()][list_of_terms.size()];
+
+        for(int i=0;i<documents_array.size();i++){
+            String page=documents_array.get(i).toLowerCase();
+            String terms[]=tokenize(page);
+
+            double maxTfThisDoc=-1.0;
+            for(int j=0;j<terms.length;j++){
+                if(terms[j].isEmpty())continue;
+
+                int ti = list_of_terms.indexOf(terms[j]);
+                matrix[i][ti]++;
+
+                if(matrix[i][ti]>=maxTfThisDoc)
+                    maxTfThisDoc = matrix[i][ti];
+            }
+            for(int j=0;j<terms.length;j++){
+                if(terms[j].isEmpty())continue;
+
+                int ti = list_of_terms.indexOf(terms[j]);
+                matrix[i][ti] = matrix[i][ti]/(maxTfThisDoc*terms.length);
+
+                double numdocuments_arrayWithTerm= (double)term_frequency_in_files.get(terms[j]).size();
+                double DF = (double)documents_array.size() / numdocuments_arrayWithTerm;
+
+                matrix[i][ti] = matrix[i][ti] * Math.log(1 + DF);
+            }
+        }
+
+    }
+    public void printMatrix(){
+        for(int j=0;j<list_of_terms.size();j++){
+            System.out.print("  "+list_of_terms.get(j));
+        }
+        System.out.println();
+        for(int i=0;i<documents_array.size();i++){
+            System.out.print(i);
+            for(int j=0;j<list_of_terms.size();j++){
+                System.out.print(" "+matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
 }
